7b60180c02a42d33152b934d1b5485e85e8be9ef
Tim <tim.spiele1@freenet.de>
2023-03-01 04:43:53 +0100

    cancel double add

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ function add(c) {
     if (currentPlayer == 0) {
         if (game[c] === null) {
             document.getElementById(`field${c}`).style.backgroundImage = 'url("textures/circle.png")'
-            game[c]
+            game[c] = 0;
             currentPlayer++
         }
     }
     else if (currentPlayer == 1) {
-        document.getElementById(`field${c}`).style.backgroundImage = 'url("textures/cross.png")'
-        currentPlayer--
+        if (game[c] === null) {
+            document.getElementById(`field${c}`).style.backgroundImage = 'url("textures/cross.png")'
+            game[c] = 1;
+            currentPlayer--
+        }
     }
 }
